a00691dbf2b1ac8f981bd3ee35968d77e050cf4b
Kam-Ya <kamranyaghoubian@gmail.com>
2025-11-16 11:53:21 -0500

    added a test function for image processing

--- src/main.m
+++ src/main.m
@@ -1,5 +1,10 @@
 
+I = dicomread("1.dcm");
+figure();
+imshow(I);
+title('Original DICOM Image');
 
+test(I);
 
 
 function list = load(folder) 
@@ -38,10 +43,36 @@ function list = filter(images)
     list = size(sizeof(images));
     % apply all filters for my model
     for i = 1:sizeof(images)
-        median = medfilt(images(i));
-        open = imopen(median);
+        median = medfilt2(images(i));
+        se = strel("disk", 4);
+        open = imopen(median, se);
         hist = histeq(open);
         list(i) = hist; 
     end
 
 end
+
+
+function test(image)
+    J = rescale(image);
+    gray = im2gray(J);
+    noisy = imnoise(gray, "salt & pepper", 0.02);
+
+    median = medfilt2(noisy);
+    se = strel("disk", 4);
+    open = imopen(median, se);
+    % Sobel operator (3x3)
+    sobel_x = [-1 0 1; -2 0 2; -1 0 1];
+    sobel_y = [1 2 1; 0 0 0; -1 -2 -1];
+    sobel_mag = abs(imfilter(open, sobel_x)) + abs(imfilter(open, sobel_y));
+    % Display the original and filtered images for comparison
+    figure;
+    subplot(2, 3, 1); imshow(gray); title("original");
+    subplot(2, 3, 2); imshow(noisy); title("noisy");
+    subplot(2, 3, 3); imshow(median); title("median filtered");
+    subplot(2, 3, 4); imshow(open); title("opened");
+    subplot(2, 3, 5); imshow(sobel_mag); title("Sobel magnitude");
+end
+    
+
+
