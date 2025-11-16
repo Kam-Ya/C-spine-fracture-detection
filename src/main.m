d52ae44b8f6e5eb76981cecf9125bf5cd9b75049
Kam-Ya <kamranyaghoubian@gmail.com>
2025-11-16 14:21:09 -0500

    added outline for general model work

--- src/main.m
+++ src/main.m
@@ -1,11 +1,38 @@
 
-I = dicomread("1.dcm");
+I = dicomread("360.dcm");
 figure();
 imshow(I);
 title('Original DICOM Image');
 
 test(I);
 
+return;
+
+% The next bit is to work on the outline of a model as I am having issues
+% with the dataset
+
+% Read data labels from csv file
+csv = "some.csv";
+labels = readtable(csv);
+
+% get images from file and make folds
+rng(0, "twister") % for reproducability 
+file = "IMAGE LOCATIONS";
+contents = dir(fullfile(file));
+c = cvpartition(numel(content), KFold=10);
+
+
+
+% work on the model
+fractures = labels.patient_overall;
+imds = imageDatastore(fractures(:,2:end));
+% going to ignore boudning box because im not sure how that would work in
+% image slices
+
+% make the options for training
+options = trainingOptions('sgdm', 'MiniBatchSize', 10, InitialLearnRate', 1e-3, MaxEpochs', 10,CheckpointPath', tempdir);
+% Train the model using the image datastore and specified options
+model = trainFastRCNNObjectDetector(imds, layers, options);
 
 function list = load(folder) 
     % define the type of file to be expected
